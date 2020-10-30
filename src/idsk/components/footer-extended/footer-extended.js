daae1a94d16f943fc0504e6ceb468e733c350923
Viliam Dlugos <viliam.dlugos@slovenskoit.sk>
2020-10-30 11:57:14 +0100

    footer-extended parametrizing, character count

--- src/idsk/components/footer-extended/footer-extended.js
+++ src/idsk/components/footer-extended/footer-extended.js
@@ -2,6 +2,62 @@ import '../../../govuk/vendor/polyfills/Function/prototype/bind'
 import '../../../govuk/vendor/polyfills/Event' // addEventListener and event.target normaliziation
 import { nodeListForEach } from '../../../govuk/common'
 import { toggleClass } from '../../common'
+// import { characterCount } from '../character-count/character-count'
+
+import  CharacterCount  from '../../../govuk/components/character-count/character-count'
+
+
+CharacterCount.prototype.updateCountMessage = function () {
+    var countElement = this.$textarea
+    var options = this.options
+    var countMessage = this.countMessage
+  
+    // Determine the remaining number of characters/words
+    var currentLength = this.count(countElement.value)
+    var maxLength = this.maxLength
+    var remainingNumber = maxLength - currentLength
+  
+    // Set threshold if presented in options
+    var thresholdPercent = options.threshold ? options.threshold : 0
+    var thresholdValue = maxLength * thresholdPercent / 100
+    if (thresholdValue > currentLength) {
+      countMessage.classList.add('govuk-character-count__message--disabled')
+      // Ensure threshold is hidden for users of assistive technologies
+      countMessage.setAttribute('aria-hidden', true)
+    } else {
+      countMessage.classList.remove('govuk-character-count__message--disabled')
+      // Ensure threshold is visible for users of assistive technologies
+      countMessage.removeAttribute('aria-hidden')
+    }
+  
+    // Update styles
+    if (remainingNumber < 0) {
+      countElement.classList.add('govuk-textarea--error')
+      countMessage.classList.remove('govuk-hint')
+      countMessage.classList.add('govuk-error-message')
+    } else {
+      countElement.classList.remove('govuk-textarea--error')
+      countMessage.classList.remove('govuk-error-message')
+      countMessage.classList.add('govuk-hint')
+    }
+  
+    // Update message
+    var charVerb = ''
+    var charNoun = 'znak'
+    var displayNumber = remainingNumber
+    if (options.maxwords) {
+      charNoun = 'slov'
+    }
+    charNoun = charNoun + ((remainingNumber === -1 || remainingNumber === 1) ? '' : 'ov')
+    //charNoun = charNoun + ((remainingNumber >1 && remainingNumber < 5) ? 'y' : '')
+    
+
+    // charVerb = (remainingNumber < 0) ? 'too many' : 'remaining'
+    displayNumber = Math.abs(remainingNumber)
+  
+    countMessage.innerHTML = 'Zostáva Vám ' + displayNumber + ' ' + charNoun + ' '
+  }
+
 
 /**
  * Footer for extended websites
@@ -10,8 +66,6 @@ class FooterExtended {
     constructor($module) {
         this.$module = $module;
     }
-
-
 
     init() {
         console.log('test');
@@ -29,11 +83,16 @@ class FooterExtended {
         let $closeErrorFormButton = $module.querySelector('#close-error-form-button');
         let $closeHelpFormButton = $module.querySelector('#close-help-form-button');
 
+        let $writeUsButton = this.$module.querySelector('#idsk-write-us-button');
 
         if ($yesButton && $noButton && $errorButton) {
             $yesButton.addEventListener('click', this.handleYesButtonClick.bind(this));
             $noButton.addEventListener('click', this.handleNoButtonClick.bind(this));
             $errorButton.addEventListener('click', this.handleErrorButtonClick.bind(this));
+        }
+
+        if ($writeUsButton) {
+            $writeUsButton.addEventListener('click', this.handleErrorButtonClick.bind(this));
         }
 
         if ($closeHelpFormButton) {
@@ -44,48 +103,7 @@ class FooterExtended {
             $closeErrorFormButton.addEventListener('click', this.handleCloseErrorFormButtonClick.bind(this));
         }
 
-
-
-
-
-
-        // // check for search component
-
-        // let $toggleSearchComponent = $module.querySelector('.idsk-header-extended__info-search');
-        // let $toggleSearchInputComponent = $module.querySelector('.idsk-header-extended__info-search-form input');
-        // if ($toggleSearchComponent && $toggleSearchInputComponent) {
-        //     // Handle $toggleSearchComponent click and blur events
-        //     $toggleSearchComponent.addEventListener('focus', this.handleSearchComponentClick.bind(this));
-        //     // both blur events needed
-        //     // if form is shown, but has not been focused, inputs blur won't be fired, then trigger this one
-        //     $toggleSearchComponent.addEventListener('blur', this.handleSearchComponentClick.bind(this));
-        //     // if form is shown, and has been focused, trigger this one
-        //     $toggleSearchInputComponent.addEventListener('blur', this.handleSearchComponentClick.bind(this));
-        // }
-
-        // // check for language selector
-        // let $toggleLanguageSelector = $module.querySelector('.idsk-js-header-extended-language-toggle');
-        // if ($toggleLanguageSelector) {
-        //     // Handle $toggleLanguageSelect click events
-        //     $toggleLanguageSelector.addEventListener('focus', this.handleLanguageSelectorClick.bind(this));
-        //     $toggleLanguageSelector.addEventListener('blur', this.handleLanguageSelectorClick.bind(this));
-        // }
-
-        // // check for submenu
-        // let $toggleSubmenus = $module.querySelectorAll('.idsk-header-extended__link');
-        // if ($toggleSubmenus) {
-        //     let $self = this;
-        //     // Handle $toggleSubmenu click events
-        //     nodeListForEach($toggleSubmenus, function ($toggleSubmenu) {
-        //         $toggleSubmenu.addEventListener('focus', $self.handleSubmenuClick.bind($self));
-        //         $toggleSubmenu.addEventListener('blur', $self.handleSubmenuClick.bind($self));
-        //     })
-        // }
-
     }
-
-
-
 
 
     hideAllElements() {
@@ -108,11 +126,8 @@ class FooterExtended {
         let $errorOption = this.$module.querySelector('#idsk-error-form');
 
 
-        
+
         let $infoQuestion = this.$module.querySelector('#idsk-info-question');
-        let $yesButton = this.$module.querySelector('#idsk-yes-button');
-        let $noButton = this.$module.querySelector('#idsk-no-button');
-        let $errorButton = this.$module.querySelector('#idsk-error-button');
         let $heartSymbol = this.$module.querySelector('#idsk-heart');
 
         //this.addClass($yesOption, 'idsk-display-none');
@@ -126,69 +141,66 @@ class FooterExtended {
         // this.toggleClass($errorButton, 'idsk-display-none');
 
         this.toggleClass($heartSymbol, 'idsk-heart-visible');
-        
+
         // let $infoQuestion = this.$module.querySelector('#idsk-info-question'); 
-        $infoQuestion.innerText = 'Ďakujeme za Vašu spätnú väzbu   '
+        // $infoQuestion.innerText = 'Ďakujeme za Vašu spätnú väzbu   '
 
-        //let $feedbackQuestion = this.$module.querySelector('#feedback-question');
-        //this.toggleClass($yesOption, 'idsk-display-none');
-        //this.toggleClass($feedbackQuestion, 'idsk-display-none');
-        //this.toggleClass($yesOption, 'idsk-open');
-
-
-
-        // console.log('yes');
     }
-    // handleNoButtonClick(e) {
-    //     let $noOption = this.$module.querySelector('#idsk-help-form');
-    //     //btn = document.querySelector('button');
-
-
-    //     if ($noOption.classList.contains('idsk-display-none')) {
-    //         $noOption.classList.remove('idsk-display-none');
-    //         setTimeout(function () {
-    //             $noOption.classList.remove('idsk-visually-hidden');
-    //         }, 20);
-    //     } else {
-    //         $noOption.classList.add('idsk-visually-hidden');
-    //         $noOption.addEventListener('transitionend', function (e) {
-    //             $noOption.classList.add('idsk-display-none');
-    //         }, {
-    //             capture: false,
-    //             once: true,
-    //             passive: false
-    //         });
-    //     }
-
-
-    // }
+    
 
     handleNoButtonClick(e) {
+        let $errorOption = this.$module.querySelector('#idsk-error-form');
+        let $helpOption = this.$module.querySelector('#idsk-help-form');
 
-        let $noOption = this.$module.querySelector('#idsk-help-form');
         let $feedbackQuestion = this.$module.querySelector('#feedback-question');
-        this.toggleClass($noOption, 'idsk-display-none');
-        this.toggleClass($noOption, 'idsk-open');
+
+        // this.addClass($errorOption, 'idsk-display-none');
+        // this.removeClass($errorOption, 'idsk-open');
+
+
+
+        //this.delay(2000).toggleClass($helpOption, 'idsk-display-none');
+        // window.setTimeout(function() {
+        //     this.toggleClass('idsk-display-none');
+        // }, 2000);
         
+        this.toggleClass($feedbackQuestion, 'idsk-display-');
 
+        this.toggleClass($helpOption, 'idsk-display-none');
 
-        //this.toggleClass($feedbackQuestion,'idsk-feedback-hidden' );
-        console.log('yes');
+        this.toggleClass($helpOption, 'idsk-open');
     }
 
     handleErrorButtonClick(e) {
         let $errorOption = this.$module.querySelector('#idsk-error-form');
+        let $helpOption = this.$module.querySelector('#idsk-help-form');
         let $feedbackQuestion = this.$module.querySelector('#feedback-question');
+        
+        
+        this.toggleClass($feedbackQuestion, 'idsk-display-');
+
+
+        this.addClass($helpOption, 'idsk-display-none');
+        this.removeClass($helpOption, 'idsk-open');
+
         this.toggleClass($errorOption, 'idsk-display-none');
         this.toggleClass($errorOption, 'idsk-open');
 
-        //this.toggleClass($feedbackQuestion,'idsk-feedback-hidden' );    
+
+        // this.toggleClass($writeUsOption, 'idsk-display-none');
+        // this.toggleClass($writeUsOption, 'idsk-open');
+        
+
     }
 
     handleCloseErrorFormButtonClick(e) {
         let $errorOption = this.$module.querySelector('#idsk-error-form');
         let $feedbackQuestion = this.$module.querySelector('#feedback-question');
         //this.toggleClass($errorOption, 'idsk-feedback-hidden');
+       
+        this.toggleClass($feedbackQuestion, 'idsk-open');
+        this.toggleClass($feedbackQuestion, 'idsk-display-');
+        
         this.toggleClass($errorOption, 'idsk-open');
         this.toggleClass($errorOption, 'idsk-display-none');
         //this.toggleClass($feedbackQuestion,'idsk-feedback-hidden' );    
@@ -197,13 +209,18 @@ class FooterExtended {
     handleCloseHelpFormButtonClick() {
         let $helpOption = this.$module.querySelector('#idsk-help-form');
         let $feedbackQuestion = this.$module.querySelector('#feedback-question');
+        
+        this.toggleClass($feedbackQuestion, 'idsk-open');
+        this.toggleClass($feedbackQuestion, 'idsk-display-');
         this.toggleClass($helpOption, 'idsk-open');
         this.toggleClass($helpOption, 'idsk-display-none');
         //this.toggleClass($feedbackQuestion,'idsk-feedback-hidden' );  
     }
 
     addClass(node, className) {
-        node.className += ' ' + className
+        if (node.className.indexOf(className) == -1) {
+            node.className += ' ' + className
+        }
     }
 
     toggleClass(node, className) {
@@ -214,55 +231,11 @@ class FooterExtended {
         }
     }
 
-    /**
-     * Handle focus/blur on search component - show/hide search form, hide/show search text wrapper
-     * @param {object} e 
-     */
-    handleSearchComponentClick(e) {
-        let $el = e.target || e.srcElement;
-        let $target = $el.closest('.idsk-header-extended__info-search');
-        let $relatedTarget = e.relatedTarget ? (e.relatedTarget).closest('.idsk-header-extended__info-search-form') : e.relatedTarget;
-        let $searchText = $target.querySelector('.idsk-header-extended__info-search-text');
-        let $searchForm = $target.querySelector('.idsk-header-extended__info-search-form');
-        if ($searchText && $searchForm) {
-            if (e.type === 'focus') {
-                $searchText.classList.add('--hide');
-                $searchForm.classList.add('--show');
-            } else if (e.type === 'blur' && $relatedTarget !== $searchForm) {
-                $searchText.classList.remove('--hide');
-                $searchForm.classList.remove('--show');
-            }
+    removeClass(node, className) {
+        if (node.className.indexOf(className) > 0) {
+            node.className = node.className.replace(' ' + className, '')
         }
     }
-
-    /**
-     * Handle open/hide language switcher
-     * @param {object} e 
-     */
-    handleLanguageSelectorClick(e) {
-        let $toggleButton = e.target || e.srcElement;
-        let $target = $toggleButton.closest('.idsk-header-extended__info-language');
-        if ($target) {
-            toggleClass($target, 'idsk-header-extended__info-language--active');
-        }
-    }
-
-    /**
-     * Handle open/hide submenu
-     * @param {object} e 
-     */
-    handleSubmenuClick(e) {
-        let $srcEl = e.target || e.srcElement;
-        let $toggleButton = $srcEl.closest('.idsk-header-extended__navigation-item');
-        toggleClass($toggleButton, 'idsk-header-extended__navigation-item--active');
-        let $target = $toggleButton
-            .closest('.idsk-header-extended__navigation-item')
-            .querySelector('.idsk-header-extended__navigation-submenu');
-        if ($target) {
-            toggleClass($target, 'idsk-header-extended__navigation-submenu--open');
-        }
-    }
-
 }
 
 export default FooterExtended
