b6a18185e6315dbec6537eea1f09f315d6654145
LubosKat <lubos.katuscak@slovenskoit.sk>
2021-01-08 16:34:15 +0100

    added mobile version

--- src/idsk/components/in-page-navigation/in-page-navigation.js
+++ src/idsk/components/in-page-navigation/in-page-navigation.js
@@ -1,6 +1,6 @@
 import '../../../govuk/vendor/polyfills/Function/prototype/bind'
 import '../../../govuk/vendor/polyfills/Event' // addEventListener and event.target normaliziation
-
+import { toggleClass } from '../../common'
 
 function InPageNavigation($module) {
     this.$module = $module
@@ -21,26 +21,44 @@ InPageNavigation.prototype.init = function () {
 
     // Handle $link click events
     $links.forEach($link => {
-        $link.addEventListener('click', this.handleClick.bind(this))
+        $link.addEventListener('click', this.handleClickLink.bind(this))
     })
+
+    let $linkPanelButton = $module.querySelector('.idsk-in-page-navigation__link-panel')
+    if (!$linkPanelButton) {
+        return
+    }
+    $linkPanelButton.addEventListener('click', this.handleClickLinkPanel.bind(this))
+
 }
 
 /**
-* An event handler for click event on $link - remove current active state and add them to the new one
+* An event handler for click event on $link - add actual title to link panel
 * @param {object} event event
 */
-InPageNavigation.prototype.handleClick = function (event) {
+InPageNavigation.prototype.handleClickLink = function (event) {
 
     let $module = this.$module
     let $link = event.target || event.srcElement
     let $items = $module.querySelectorAll('.idsk-in-page-navigation__list-item')
-    let $currentLink = $module.querySelector('.idsk-in-page-navigation__current-link')
+    let $linkPanelText = $module.querySelector('.idsk-in-page-navigation__link-panel-button')
 
     $items.forEach($item => {
         $item.classList.remove('idsk-in-page-navigation__list-item--active')
     })
     $link.closest('li.idsk-in-page-navigation__list-item').classList.add('idsk-in-page-navigation__list-item--active')
-    $currentLink.innerText = $link.innerText;
+    $linkPanelText.innerText = $link.innerText;
+}
+
+/**
+* An event handler for click event on $linkPanel - collapse or expand in page navigation menu
+ * @param {object} event 
+ */
+InPageNavigation.prototype.handleClickLinkPanel = function (event) {
+
+    let $module = this.$module
+    let $list = $module.querySelector('.idsk-in-page-navigation__list')
+    toggleClass($list, 'idsk-in-page-navigation__list--expand')
 }
 
 export default InPageNavigation
