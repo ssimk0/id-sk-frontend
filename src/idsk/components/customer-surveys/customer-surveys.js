516c78fe4f474a99aee3e77d8c5820e40595b451
Viliam Dlugos <viliam.dlugos@slovenskoit.sk>
2021-02-23 21:32:22 +0100

    customer-surveys: step logic

--- src/idsk/components/customer-surveys/customer-surveys.js
+++ src/idsk/components/customer-surveys/customer-surveys.js
@@ -4,31 +4,36 @@ import { nodeListForEach, toggleClass } from "../../common";
 
 function CustomerSurveys($module) {
     this.$module = $module;
-    this.$items = $module.querySelectorAll(".idsk-crossroad-title");
 }
-
-
 
 CustomerSurveys.prototype.init = function () {
     var $module = this.$module;
-    var $items = this.$items;
-    var $uncollapseButton = $module.querySelector('#idsk-crossroad__uncollapse-button');
+    var $nextButton = $module.querySelector('#idsk-customer-surveys__send-button');
 
-    if (!$module || !$items) {
+    if (!$module) {
         return;
     }
 
-    if ($uncollapseButton) {
-        $uncollapseButton.addEventListener('click', this.handleShowItems.bind(this));
+    if ($nextButton) {
+        $nextButton.addEventListener('click', this.handleNextButtonClick.bind(this));
     }
 
-    nodeListForEach(
-        $items,
-        function ($item) {
-            $item.addEventListener("click", this.handleItemClick.bind(this));
-        }.bind(this)
-    );
 };
+
+CustomerSurveys.prototype.handleNextButtonClick = function (e) {
+    var $steps = this.$module.querySelectorAll('.idsk-customer-surveys__step');
+    var i;
+
+    for (i = 0; i < $steps.length-1; i++) {
+        if ($steps[i].classList.contains('idsk-customer-surveys--show')) {
+            $steps[i].classList.remove('idsk-customer-surveys--show');
+            toggleClass($steps[i], 'idsk-customer-surveys--hidden');
+            toggleClass($steps[i+1], 'idsk-customer-surveys--hidden');
+            $steps[i+1].classList.add('idsk-customer-surveys--show');
+            return;
+        }
+    };
+}
 
 
 export default CustomerSurveys;
