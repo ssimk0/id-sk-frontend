d326abfb29c3075a3cb943e77cf0b79a70f2893e
Viliam Dlugos <viliam.dlugos@slovenskoit.sk>
2021-03-30 10:32:19 +0200

    search-results: handling disable filters, paging

--- src/idsk/components/search-results/search-results.js
+++ src/idsk/components/search-results/search-results.js
@@ -12,6 +12,9 @@ SearchResults.prototype.init = function () {
     $module.resultCards = new Array();
     $module.countOfCardsPerPage = new Number();
     $module.currentPageNumber = new Number();
+    $module.subTopicButton = $module.querySelector('.idsk-search-results__subtopic')
+
+    $module.subTopicButton.disabled = true
 
     if (!$module) {
         return
@@ -55,7 +58,6 @@ SearchResults.prototype.init = function () {
 
     $backButton.addEventListener('click', this.handleClickPreviousPage.bind(this))
     $forwardButton.addEventListener('click', this.handleClickNextPage.bind(this))
-
     $module.boundHandleClickLinkPanel = this.handleClickLinkPanel.bind(this)
 
     // set selected value in dropdown
@@ -73,11 +75,6 @@ SearchResults.prototype.init = function () {
     $radioButtonsInput.forEach(function ($input) {
         $input.addEventListener('click', this.handleClickRadioButton.bind(this), true)
     }.bind(this))
-
-
-
-    // Handle case if the viewport is shor and there are more than one article - scrolling is not needed, but navigation pointer has to be updated
-    this.$module.labelChanged = false
 }
 
 SearchResults.prototype.handleClickPreviousPage = function (e) {
@@ -85,33 +82,29 @@ SearchResults.prototype.handleClickPreviousPage = function (e) {
     var $module = this.$module
 
     $module.currentPageNumber = $module.currentPageNumber - 1
-
-    this.showResultCardsPerPage.call(this, $module.countOfCardsPerPage * ($module.currentPageNumber - 1) , $module.countOfCardsPerPage * ($module.currentPageNumber ));
-
-    //$module.countOfCardsPerPage = $el.value
+    this.showResultCardsPerPage.call(this, $module.countOfCardsPerPage * ($module.currentPageNumber - 1), $module.countOfCardsPerPage * $module.currentPageNumber);
 }
 
 SearchResults.prototype.handleClickNextPage = function (e) {
     var $el = e.target || e.srcElement
     var $module = this.$module
 
-    console.log($module.countOfCardsPerPage)
     $module.currentPageNumber = $module.currentPageNumber + 1
-    this.showResultCardsPerPage.call(this, $module.countOfCardsPerPage, $module.countOfCardsPerPage * 2);
-
-    // $module.countOfCardsPerPage = $el.value
+    this.showResultCardsPerPage.call(this, $module.countOfCardsPerPage * ($module.currentPageNumber - 1), $module.countOfCardsPerPage * $module.currentPageNumber);
 }
 
 SearchResults.prototype.handleClickResultsPerPageDropdown = function (e) {
     var $el = e.target || e.srcElement
     var $module = this.$module
 
-
-    this.showResultCardsPerPage.call(this, 0, $el.value);
-
     $module.countOfCardsPerPage = $el.value
+    this.showResultCardsPerPage.call(this, 0, $el.value);
 }
 
+/**
+ * function for changing number of cards on page
+ * 
+ */
 SearchResults.prototype.showResultCardsPerPage = function ($startIndex, $endIndex) {
     var $module = this.$module
     var $backButton = $module.querySelector('.idsk-search-results__button--back')
@@ -146,12 +139,8 @@ SearchResults.prototype.showResultCardsPerPage = function ($startIndex, $endInde
         $backButton.classList.add('idsk-search-results--invisible')
     }
 
-    var $numberOfPages = ($module.resultCards.length / ($endIndex - $startIndex)) | 0
-    if ($module.resultCards.length % $endIndex > 0) {
-        $numberOfPages = $numberOfPages + 1
-    }
+    var $numberOfPages = (($module.resultCards.length / $module.countOfCardsPerPage) | 0) + 1
     $pageNumber.innerText = 'Strana ' + $module.currentPageNumber + ' z ' + $numberOfPages
-
 }
 
 /**
@@ -160,7 +149,6 @@ SearchResults.prototype.showResultCardsPerPage = function ($startIndex, $endInde
  */
 SearchResults.prototype.handleClickLinkPanel = function (e) {
     var $el = e.target || e.srcElement
-    var $module = this.$module
     var $linkPanelButton = $el.closest('.idsk-search-results__link-panel')
     var $contentPanel = $linkPanelButton.querySelector('.idsk-search-results__list')
 
@@ -177,17 +165,14 @@ SearchResults.prototype.handleClickRadioButton = function (e) {
     var $module = this.$module
     var $linkPanelButton = $el.closest('.idsk-search-results__link-panel')
     var $buttonCaption = $linkPanelButton.querySelector('.idsk-search-results__link-panel--span')
-
     var $choosenFiltersContainer = $module.querySelector('.idsk-search-results__content__picked-filters__topics')
     var $filterContainer = $choosenFiltersContainer.querySelector('.idsk-search-results__picked-topic')
-
     var $radios = $el.closest('.govuk-radios')
 
     // creating or renaming new span element for picked topic
-
-
     if ($el.value && !$filterContainer) {
         var $topicPicked = this.createTopicInContainer.call(this, $choosenFiltersContainer, $radios, $el);
+        $module.subTopicButton.disabled = false;
     } else if ($filterContainer.dataset.source == $radios.dataset.id) {
         $topicPicked = $choosenFiltersContainer.querySelector('.idsk-search-results__picked-topic');
         $topicPicked.innerHTML = $el.value + ' &#10005;';
@@ -197,7 +182,6 @@ SearchResults.prototype.handleClickRadioButton = function (e) {
 
     $topicPicked.removeEventListener('click', this.handleRemovePickedTopic.bind(this), true);
     $topicPicked.addEventListener('click', this.handleRemovePickedTopic.bind(this));
-
     $buttonCaption.innerText = '1 vybraté'
 }
 
@@ -207,12 +191,29 @@ SearchResults.prototype.createTopicInContainer = function ($choosenFiltersContai
     $topicPicked.setAttribute('data-source', $radios.dataset.id)
     $topicPicked.innerHTML = $el.value + ' &#10005;';
     $choosenFiltersContainer.appendChild($topicPicked);
-
     return $topicPicked
 }
 
 SearchResults.prototype.handleRemovePickedTopic = function (e) {
     var $el = e.target || e.srcElement
+    var $choosenFiltersContainer = this.$module.querySelector('.idsk-search-results__content__picked-filters__topics')
+
+    if ($el.dataset.source == 'tema') {
+        var $subTopic = $choosenFiltersContainer.querySelector('[data-source="podtema"]')
+        if ($subTopic) {
+            this.removeTopic.call(this, $subTopic, true);
+        } else {
+            this.$module.subTopicButton.parentElement.classList.remove('idsk-search-results--expand')
+            var $contentPanel = this.$module.subTopicButton.parentElement.querySelector('.idsk-search-results__list')
+            $contentPanel.classList.add('idsk-search-results--hidden')
+            this.$module.subTopicButton.disabled = true;
+        }
+    }
+
+    this.removeTopic.call(this, $el, false);
+}
+
+SearchResults.prototype.removeTopic = function ($el, $disableFilter) {
     var $radios = this.$module.querySelector('[data-id="' + $el.dataset.source + '"]')
     var $buttonCaption = $radios.closest('.idsk-search-results__link-panel')
 
@@ -220,6 +221,13 @@ SearchResults.prototype.handleRemovePickedTopic = function (e) {
     $radios.querySelectorAll('.govuk-radios__input').forEach(function ($radio) {
         $radio.checked = false
     }.bind(this))
+
+    if ($disableFilter) {
+        this.$module.subTopicButton.parentElement.classList.remove('idsk-search-results--expand')
+        var $contentPanel = this.$module.subTopicButton.parentElement.querySelector('.idsk-search-results__list')
+        $contentPanel.classList.add('idsk-search-results--hidden')
+        this.$module.subTopicButton.disabled = true;
+    }
 
     $el.remove();
 }
