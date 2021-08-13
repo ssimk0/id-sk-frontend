0e8edb6b93ddbb7e60179de8ac7d4753bdef7593
Adrián Kóša <adrian.kosa@slovenskoit.sk>
2021-08-13 13:11:50 +0200

    refactor after code review cookies settings

--- app/assets/js/nastavenia-cookies.js
+++ app/assets/js/nastavenia-cookies.js
@@ -1,88 +1,44 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-    //Init function 
-    var gaCheckbox = document.getElementById("ga-cookies");
-    var preferencesCheckbox = document.getElementById("preferences-cookies");
+//Init function 
+var gaCheckbox = document.getElementById("ga-cookies");
+var preferencesCheckbox = document.getElementById("preferences-cookies");
 
-    (function intCookies(){
-        //Check if Google Analytics exist in localStorage if not create key and add default value to true
-        if(!(window.localStorage.getItem('googleAnalytics'))){window.localStorage.setItem('googleAnalytics', 'true');}
-
-        if(window.localStorage.getItem('googleAnalytics') == 'true'){
-            addGa();
-            if(gaCheckbox && !(window.getComputedStyle(gaCheckbox.nextElementSibling, ':after').getPropertyValue('opacity') == 1)){
-                gaCheckbox.setAttribute("checked"); 
-                }
-        }else{
-            if(gaCheckbox){
-                gaCheckbox.removeAttribute("checked"); 
+(function intCookies(){
+    if(window.localStorage.getItem('googleAnalytics') == 'true'){
+        if(gaCheckbox && !(window.getComputedStyle(gaCheckbox.nextElementSibling, ':after').getPropertyValue('opacity') == 1)){
+            gaCheckbox.setAttribute("checked"); 
             }
-        }
-
-        if(window.localStorage.getItem('preferences') == 'true'){
-            addPreferences();
-            if(preferencesCheckbox && !(window.getComputedStyle(preferencesCheckbox.nextElementSibling, ':after').getPropertyValue('opacity') == 1)){
-                preferencesCheckbox.setAttribute("checked", "checked"); 
-                }
-        }else{
-            if(preferencesCheckbox){
-                preferencesCheckbox.removeAttribute("checked"); 
-            }
-        }
-    })();
-
-    function addGa(){
-        //Google analytics scripts
-        var gaScriptSrc = document.createElement("script");
-        gaScriptSrc.async = true;
-        gaScriptSrc.id = "ga-script-src";
-        gaScriptSrc.src = "https://www.googletagmanager.com/gtag/js?id=G-WR6TZ5RBNF";
-
-        var gaScript = document.createElement("script");
-        gaScript.id = "ga-script"
-        gaScript.innerHTML = 
-        `window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-WR6TZ5RBNF');`;
-
-        var head = document.getElementsByTagName('head')[0];
-        head.insertBefore(gaScript, head.firstChild);
-        head.insertBefore(gaScriptSrc, head.firstChild);
-    }
-
-    function removeGa(){
-        var headGaScript = document.getElementById("ga-script");
-        if(headGaScript){
-            (elem = headGaScript).parentNode.removeChild(elem);
-            (elem = document.getElementById("ga-script-src")).parentNode.removeChild(elem);
+    }else{
+        if(gaCheckbox){
+            gaCheckbox.removeAttribute("checked"); 
         }
     }
+    if(window.localStorage.getItem('preferences') == 'true'){
+        if(preferencesCheckbox && !(window.getComputedStyle(preferencesCheckbox.nextElementSibling, ':after').getPropertyValue('opacity') == 1)){
+            preferencesCheckbox.setAttribute("checked", "checked"); 
+            }
+    }else{
+        if(preferencesCheckbox){
+            preferencesCheckbox.removeAttribute("checked"); 
+        }
+    }
+})();
 
-    function addPreferences(){
+//Handle save cookie preferencies button
+var saveCookieButton = document.getElementsByClassName("save-cookie-settings")[0];
+if(saveCookieButton){
+    
+    saveCookieButton.onclick = function () { 
+    if(window.getComputedStyle(gaCheckbox.nextElementSibling, ':after').getPropertyValue('opacity') == 1){
+        window.localStorage.setItem('googleAnalytics', 'true');
+    }else{
+        window.localStorage.setItem('googleAnalytics', 'false'); 
+    }
+
+    if(window.getComputedStyle(preferencesCheckbox.nextElementSibling, ':after').getPropertyValue('opacity') == 1){
         window.localStorage.setItem('preferences', 'true');
-    }
-
-    function removePreferences(){
+    }else{
         window.localStorage.setItem('preferences', 'false'); 
     }
-
-    //Handle save cookie preferencies button
-    var saveCookieButton = document.getElementsByClassName("save-cookie-settings")[0];
-    if(saveCookieButton){
-        
-        saveCookieButton.onclick = function () { 
-        if(window.getComputedStyle(gaCheckbox.nextElementSibling, ':after').getPropertyValue('opacity') == 1){
-            window.localStorage.setItem('googleAnalytics', 'true');
-        }else{
-            window.localStorage.setItem('googleAnalytics', 'false'); 
-        }
-
-        if(window.getComputedStyle(preferencesCheckbox.nextElementSibling, ':after').getPropertyValue('opacity') == 1){
-            window.localStorage.setItem('preferences', 'true');
-        }else{
-            window.localStorage.setItem('preferences', 'false'); 
-        }
-        location.reload();
-        }
+    location.reload();
     }
-});
+}
