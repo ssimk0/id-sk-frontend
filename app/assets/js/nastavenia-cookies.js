e58728039175df925050415e5b12d9dae7821efd
Adrián Kóša <adrian.kosa@slovenskoit.sk>
2021-08-16 10:50:02 +0200

    cookies settings refactor code

--- app/assets/js/nastavenia-cookies.js
+++ app/assets/js/nastavenia-cookies.js
@@ -3,42 +3,16 @@ var gaCheckbox = document.getElementById("ga-cookies");
 var preferencesCheckbox = document.getElementById("preferences-cookies");
 
 (function intCookies(){
-    if(window.localStorage.getItem('googleAnalytics') == 'true'){
-        if(gaCheckbox && !(gaCheckbox.checked)){
-            gaCheckbox.setAttribute("checked"); 
-            }
-    }else{
-        if(gaCheckbox){
-            gaCheckbox.removeAttribute("checked"); 
-        }
-    }
-    if(window.localStorage.getItem('preferences') == 'true'){
-        if(preferencesCheckbox && !(preferencesCheckbox.checked)){
-            preferencesCheckbox.setAttribute("checked", "checked"); 
-            }
-    }else{
-        if(preferencesCheckbox){
-            preferencesCheckbox.removeAttribute("checked"); 
-        }
-    }
+    gaCheckbox.checked = (window.localStorage.getItem('googleAnalytics') === 'true');   
+    preferencesCheckbox.checked = (window.localStorage.getItem('preferences') === 'true');
 })();
 
 //Handle save cookie preferencies button
 var saveCookieButton = document.getElementsByClassName("save-cookie-settings")[0];
 if(saveCookieButton){
-    
     saveCookieButton.onclick = function () { 
-    if(gaCheckbox.checked){
-        window.localStorage.setItem('googleAnalytics', 'true');
-    }else{
-        window.localStorage.setItem('googleAnalytics', 'false'); 
-    }
-
-    if(preferencesCheckbox.checked){
-        window.localStorage.setItem('preferences', 'true');
-    }else{
-        window.localStorage.setItem('preferences', 'false'); 
-    }
+        window.localStorage.setItem('googleAnalytics', gaCheckbox.checked);
+        window.localStorage.setItem('preferences', preferencesCheckbox.checked);
     location.reload();
     }
 }
