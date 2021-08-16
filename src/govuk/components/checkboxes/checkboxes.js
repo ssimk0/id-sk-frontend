7e85489cb46aa99794315b39adcd116f3dfc6376
Adrián Kóša <adrian.kosa@slovenskoit.sk>
2021-08-16 11:13:21 +0200

    chceckboxes remove debuger

--- src/govuk/components/checkboxes/checkboxes.js
+++ src/govuk/components/checkboxes/checkboxes.js
@@ -37,7 +37,6 @@ Checkboxes.prototype.init = function () {
 }
 
 Checkboxes.prototype.setAttributes = function ($input) {
-  debugger;
   var inputIsChecked = $input.checked
   $input.setAttribute('aria-expanded', inputIsChecked)
 
@@ -49,7 +48,6 @@ Checkboxes.prototype.setAttributes = function ($input) {
 
 Checkboxes.prototype.handleClick = function (event) {
   var $target = event.target
-  debugger;
   // If a checkbox with aria-controls, handle click
   var isCheckbox = $target.getAttribute('type') === 'checkbox'
   var hasAriaControls = $target.getAttribute('aria-controls')
