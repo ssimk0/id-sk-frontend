29b45b2919245ae37be48ffc1484fe9a3daf3d5a
LubosKat <lubos.katuscak@slovenskoit.sk>
2021-09-30 12:00:03 +0200

    fixed es6 syntax in component

--- src/idsk/components/customer-surveys/customer-surveys.js
+++ src/idsk/components/customer-surveys/customer-surveys.js
@@ -221,7 +221,7 @@ CustomerSurveys.prototype.handleNextButtonClick = function (e) {
             $steps[i + 1].classList.add('idsk-customer-surveys--show');
             if (i == 4) {
                 $nextButtonText.innerHTML = $nextButtonText.dataset.line3;
-                setTimeout(() => {
+                setTimeout(function () {
                     $nextButton.setAttribute('type', 'submit');
                 }, 500);
                 toggleClass($startIcon[0], 'idsk-customer-surveys__icon--hidden');
