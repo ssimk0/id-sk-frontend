2b4d4915079a0b4d9ff7c0392869ff36da249f30
Adrián Kóša <adrian.kosa@slovenskoit.sk>
2021-10-04 08:08:03 +0200

    done ready for review

--- src/idsk/components/header-web/header-web.js
+++ src/idsk/components/header-web/header-web.js
@@ -8,8 +8,6 @@ import { toggleClass } from '../../common'
  */
 function HeaderWeb($module) {
     this.$module = $module;
-    this.$lastMenuElement = '';
-    this.$firstMenuElement = '';
 }
 
 HeaderWeb.prototype.init = function () {
@@ -18,16 +16,6 @@ HeaderWeb.prototype.init = function () {
     // check for module
     if (!$module) {
         return;
-    }
-
-    // check for search component
-    var $searchComponents = $module.querySelectorAll('.idsk-header-extended__search');
-    if ($searchComponents) {
-        nodeListForEach($searchComponents, function ($searchComponent) {
-            $searchComponent.addEventListener('change', this.handleSearchChange.bind(this))
-            // trigger change event
-            $searchComponent.dispatchEvent(new Event('change'));
-        }.bind(this))
     }
 
     // check for language switcher
@@ -45,8 +33,8 @@ HeaderWeb.prototype.init = function () {
             $lastLanguageItem.addEventListener('blur', this.checkBlurLanguageSwitcherClick.bind(this));
         }.bind(this))
 
-        //
-        $module.addEventListener('keydown', this.handleBackTabbing.bind(this));
+        // close language list if user back tabbing
+        $toggleLanguageSwitcher.addEventListener('keydown', this.handleBackTabbing.bind(this));
 
     }
 
@@ -62,66 +50,30 @@ HeaderWeb.prototype.init = function () {
     }
 
     // check for menu items
+    var $menuList = $module.querySelector('.idsk-header-web__nav-list');
     var $menuItems = $module.querySelectorAll('.idsk-header-web__nav-list-item-link');
-    if ($menuItems) {
+    if ($menuItems && $menuList) {
         // Handle $menuItem click events
         nodeListForEach($menuItems, function ($menuItem) {
             $menuItem.addEventListener('click', this.handleSubmenuClick.bind(this));
             $menuItem.addEventListener('focus', this.handleSubmenuClick.bind(this));
         }.bind(this))
+
+        this.$firstMenuItem = $menuList.firstElementChild;
+        this.$lastMenuItem = $menuList.lastElementChild;
+        $menuList.addEventListener('keydown', this.menuTabbing.bind(this));
     }
 
-    // check for menu button and close menu button
-    var $menuButton = $module.querySelector('.idsk-header-web__main-headline-menu-button');
-    if ($menuButton) {
-        $menuButton.addEventListener('click', this.showMobileMenu.bind(this));
+    // check for mobile menu button
+    var $menuButtons = $module.querySelectorAll('.idsk-header-web__main-headline-menu-button');
+    if ($menuButtons.length > 0) {
+        nodeListForEach($menuButtons, function ($menuButton) {
+            $menuButton.addEventListener('click', this.showMobileMenu.bind(this));
+            this.menuBtnText = $menuButton.innerText.trim();
+        }.bind(this))
     }
 
     $module.boundCheckBlurMenuItemClick = this.checkBlurMenuItemClick.bind(this);
-
-    // check for cookies
-
-    if (!(window.localStorage.getItem('acceptedCookieBanner'))) {
-        $module.classList.add('idsk-header-extended--cookie');
-        var $cookieBanner = document.querySelector('.idsk-cookie-banner');
-
-        // scroll handler
-        window.addEventListener('scroll', function() {
-            var headerPosition = document.body.getBoundingClientRect().top;
-            var cookieBannerHeight = $cookieBanner.offsetHeight;
-            if(headerPosition < (-cookieBannerHeight)){
-                $module.classList.remove('idsk-header-extended--cookie');
-                $module.style.top = '0px';
-            }else{
-                $module.classList.add('idsk-header-extended--cookie');
-                $module.style.top = cookieBannerHeight.toString() + 'px';
-            }
-        });
-
-        // cookie resize handler
-        var resizeObserver = new ResizeObserver(entries => {
-            $module.style.top = $cookieBanner.offsetHeight.toString() + 'px';
-          });
-
-          resizeObserver.observe($cookieBanner);
-
-    }
-    
-}
-
-/**
- * Hide label if search input is not empty
- * @param {object} e 
- */
-HeaderWeb.prototype.handleSearchChange = function (e) {
-    var $searchInput = e.target || e.srcElement;
-    var $search = $searchInput.closest('.idsk-header-extended__search')
-    var $searchLabel = $search.querySelector('label')
-    if ($searchInput.value) {
-        $searchLabel.classList.add('idsk-header-extended__search-input--focus')
-    } else {
-        $searchLabel.classList.remove('idsk-header-extended__search-input--focus')
-    }
 }
 
 /**
@@ -143,11 +95,8 @@ HeaderWeb.prototype.checkBlurLanguageSwitcherClick = function () {
 HeaderWeb.prototype.handleBackTabbing = function (e) {
     //shift was down when tab was pressed
     if(e.shiftKey && e.keyCode == 9) { 
-        var $focusedElement = this.$module.querySelector(':focus');
-
-        if ($focusedElement == this.$toggleLanguageSwitcher) {
-            var languageList = this.$module.querySelector('.idsk-header-web__brand-language-list');
-            languageList.lastElementChild.focus();
+        if (document.activeElement == this.$toggleLanguageSwitcher) {
+            this.$toggleLanguageSwitcher.parentNode.classList.remove('idsk-header-web__brand-language--active');
         }
     }
 }
@@ -171,10 +120,10 @@ HeaderWeb.prototype.handleBackTabbing = function (e) {
 HeaderWeb.prototype.handleSubmenuClick = function (e) {
     var $srcEl = e.target || e.srcElement;
     var $toggleButton = $srcEl.closest('.idsk-header-web__nav-list-item');
-    var $currActiveList = this.$module.querySelectorAll('.idsk-header-web__nav-list-item--active');
+    var $currActiveItem = this.$module.querySelector('.idsk-header-web__nav-list-item--active');
 
-    if ($currActiveList.length > 0) {
-        $currActiveList[0].classList.remove('idsk-header-web__nav-list-item--active');
+    if ($currActiveItem) {
+        $currActiveItem.classList.remove('idsk-header-web__nav-list-item--active');
     }
     toggleClass($toggleButton, 'idsk-header-web__nav-list-item--active');
 
@@ -182,12 +131,36 @@ HeaderWeb.prototype.handleSubmenuClick = function (e) {
 }
 
 /**
+ * Remove active class from menu when user leaves menu with tabbing
+ */
+ HeaderWeb.prototype.menuTabbing = function (e) {
+
+    var isTabPressed = (e.key === 'Tab' || e.keyCode === 9);
+
+    if (!isTabPressed) {
+        return;
+    }
+
+    // shift + tab
+    if (e.shiftKey) {
+        if (document.activeElement === this.$firstMenuItem.firstElementChild) {
+            this.$firstMenuItem.classList.remove('idsk-header-web__nav-list-item--active');
+        }
+    // tab
+    } else if (document.activeElement === this.$lastMenuItem.firstElementChild) {
+        this.$lastMenuItem.classList.remove('idsk-header-web__nav-list-item--active');
+    } 
+}
+
+/**
  * handle click outside menu or "blur" the item link
  */
 HeaderWeb.prototype.checkBlurMenuItemClick = function () {
-    var $currActiveList = this.$module.querySelectorAll('.idsk-header-web__nav-list-item--active');
-    $currActiveList[0].classList.remove('idsk-header-web__nav-list-item--active');
-    document.removeEventListener('click', this.$module.boundCheckBlurMenuItemClick, true);
+    var $currActiveItem = this.$module.querySelector('.idsk-header-web__nav-list-item--active');
+    if($currActiveItem){
+        $currActiveItem.classList.remove('idsk-header-web__nav-list-item--active');
+        document.removeEventListener('click', this.$module.boundCheckBlurMenuItemClick, true); 
+    }
 }
 
 /**
@@ -200,60 +173,10 @@ HeaderWeb.prototype.showMobileMenu = function (e) {
     toggleClass($mobileMenu, 'idsk-header-web__nav--mobile');
     toggleClass($menuButton, 'idsk-header-web__main-headline-menu-button--active');
     
-}
-/**
- * Hide mobile menu
- * @param {object} e
- */
-HeaderWeb.prototype.hideMobileMenu = function (e) {
-    var $hamburgerMenuButton = this.$module.querySelector('.idsk-js-header-extended-side-menu');
-
-    this.$module.classList.remove("idsk-header-extended--show-mobile-menu");
-    document.getElementsByTagName("body")[0].style.overflow = "visible";
-    $hamburgerMenuButton.focus();
-}
-
-/**
- * Create loop in mobile menu for tabbing elements
- */
-HeaderWeb.prototype.initMobileMenuTabbing = function () {
-    //Get header extended mobile menu focusable elements
-    var $headerExtended = this.$module.querySelectorAll('.idsk-header-extended__mobile')[0];
-    var $mobileMenuElements = $headerExtended.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])');
-    this.$firstMenuElement = $mobileMenuElements[0];
-    this.$lastMenuElement = $mobileMenuElements[$mobileMenuElements.length - 1];
-    var KEYCODE_TAB = 9;
-
-    document.addEventListener('keydown', function (e) {
-        var isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
-
-        if (!isTabPressed) {
-            return;
-        }
-
-        if (e.shiftKey) { // shift + tab
-            if (document.activeElement === this.$firstMenuElement) {
-                this.$lastMenuElement.focus();
-                e.preventDefault();
-            }
-        } else if (document.activeElement === this.$lastMenuElement) { // tab
-            this.$firstMenuElement.focus();
-            e.preventDefault();
-        }
-
-    }.bind(this));
-}
-
-/**
- * When the user scrolls down from the top of the document, resize the navbar's padding and the logo
- */
-HeaderWeb.prototype.scrollFunction = function () {
-    var $module = this.$module;
-
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        $module.classList.add('idsk-header-extended--shrink');
-    } else if (document.body.scrollTop < 10 && document.documentElement.scrollTop < 10) {
-        $module.classList.remove('idsk-header-extended--shrink');
+    if($menuButton.classList.contains('idsk-header-web__main-headline-menu-button--active')){
+        $menuButton.childNodes[0].nodeValue = 'Zavrieť';
+    }else{
+        $menuButton.childNodes[0].nodeValue = this.menuBtnText;
     }
 }
 
