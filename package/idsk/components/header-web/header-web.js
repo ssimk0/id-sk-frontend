b353cb4810ca96f8fdb7b7426992a1895570b036
LubosKat <lubos.katuscak@slovenskoit.sk>
2021-11-03 16:44:13 +0100

    release 2.7.0

--- package/idsk/components/header-web/header-web.js
+++ package/idsk/components/header-web/header-web.js
@@ -659,150 +659,296 @@ if (detect) return
 })
 .call('object' === typeof window && window || 'object' === typeof self && self || 'object' === typeof global && global || {});
 
-function InPageNavigation($module) {
+/**
+ * TODO: Ideally this would be a NodeList.prototype.forEach polyfill
+ * This seems to fail in IE8, requires more investigation.
+ * See: https://github.com/imagitama/nodelist-foreach-polyfill
+ */
+function nodeListForEach(nodes, callback) {
+  if (window.NodeList.prototype.forEach) {
+    return nodes.forEach(callback)
+  }
+  for (var i = 0; i < nodes.length; i++) {
+    callback.call(window, nodes[i], i, nodes);
+  }
+}
+
+/**
+ * TODO: Ideally this would be a NodeList.prototype.forEach polyfill
+ * This seems to fail in IE8, requires more investigation.
+ * See: https://github.com/imagitama/nodelist-foreach-polyfill
+ */
+
+/**
+ * Toggle class
+ * @param {object} node element
+ * @param {string} className to toggle
+ */
+function toggleClass(node, className) {
+    if (node === null) {
+        return;
+    }
+
+    if (node.className.indexOf(className) > 0) {
+        node.className = node.className.replace(' ' + className, '');
+    } else {
+        node.className += ' ' + className;
+    }
+}
+
+/**
+ * Header for web websites
+ */
+function HeaderWeb($module) {
     this.$module = $module;
 }
 
-InPageNavigation.prototype.init = function () {
-    // Check for module
+HeaderWeb.prototype.init = function () {
+
     var $module = this.$module;
+    // check for module
     if (!$module) {
-        return
+        return;
     }
 
-    // Check for button
-    var $links = $module.querySelectorAll('.idsk-in-page-navigation__link');
-    if (!$links) {
-        return
+    // chceck for banner
+    var $banner = $module.querySelector('.idsk-header-web__banner');
+    if ($banner) {
+        var $bannerCloseBtn = $banner.querySelector('.idsk-header-web__banner-close');
+        $bannerCloseBtn.addEventListener('click', this.handleCloseBanner.bind(this));
     }
 
-    // list of all ids and titles
-    this.$arrTitlesAndElems = [];
-    // Handle $link click events
-    $links.forEach(function ($link) {
-        var $item = {};
-        $item.el = document.getElementById($link.href.split('#')[1]);
-        this.$arrTitlesAndElems.push($item);
-        $link.addEventListener('click', this.handleClickLink.bind(this));
-    }.bind(this));
+    // check for language switcher
+    var $toggleLanguageSwitcher = $module.querySelector('.idsk-header-web__brand-language-button');
+    this.$toggleLanguageSwitcher = $toggleLanguageSwitcher;
 
-    var $linkPanelButton = $module.querySelector('.idsk-in-page-navigation__link-panel');
-    if (!$linkPanelButton) {
-        return
-    }
-    $module.boundCheckCloseClick = this.checkCloseClick.bind(this);
-    $module.boundHandleClickLinkPanel = this.handleClickLinkPanel.bind(this);
-    $linkPanelButton.addEventListener('click', $module.boundHandleClickLinkPanel, true);
+    if ($toggleLanguageSwitcher) {
+        // Handle $toggleLanguageSwitcher click events
+        $toggleLanguageSwitcher.addEventListener('click', this.handleLanguageSwitcherClick.bind(this));
 
-    // Handle floating navigation
-    window.addEventListener('scroll', this.scrollFunction.bind(this));
-    // Handle case if the viewport is shor and there are more than one article - scrolling is not needed, but navigation pointer has to be updated
-    this.$module.labelChanged = false;
-};
+        // close language list if i left the last item from langauge list e.g. if user use tab key for navigations
+        var $lastLanguageItems = $module.querySelectorAll('.idsk-header-web__brand-language-list-item:last-child .idsk-header-web__brand-language-list-item-link');
+        nodeListForEach($lastLanguageItems, function ($lastLanguageItem) {
+            $lastLanguageItem.addEventListener('blur', this.checkBlurLanguageSwitcherClick.bind(this));
+        }.bind(this));
 
-/**
- * An event handler for click event on $link - add actual title to link panel
- * @param {object} e
- */
-InPageNavigation.prototype.handleClickLink = function (e) {
-    var $link = e.target || e.srcElement;
-    var $id = $link.closest('.idsk-in-page-navigation__link').href.split('#')[1];
-    var $panelHeight = this.$module.getElementsByClassName('idsk-in-page-navigation__link-panel')[0].offsetHeight;
+        // close language list if user back tabbing
+        $toggleLanguageSwitcher.addEventListener('keydown', this.handleBackTabbing.bind(this));
 
-    setTimeout(function () {
-        if (document.getElementById($id) != null) {
-            this.$module.labelChanged = true;
-            this.changeCurrentLink($link);
-            window.scrollTo(0, document.getElementById($id).offsetTop - ($panelHeight * 2.5));
-        } else {
-            this.changeCurrentLink($link);
-        }
-    }.bind(this), 10);
-};
-
-/**
- * An event handler for click event on $linkPanel - collapse or expand in page navigation menu
- * @param {object} e
- */
-InPageNavigation.prototype.handleClickLinkPanel = function (e) {
-    var $module = this.$module;
-    var $linkPanelButton = $module.querySelector('.idsk-in-page-navigation__link-panel');
-
-    $module.classList.add('idsk-in-page-navigation--expand');
-    $linkPanelButton.removeEventListener('click', $module.boundHandleClickLinkPanel, true);
-    document.addEventListener('click', $module.boundCheckCloseClick, true);
-};
-
-/**
- * close navigation if the user click outside navigation
- * @param {object} e
- */
-InPageNavigation.prototype.checkCloseClick = function (e) {
-    var $el = e.target || e.srcElement;
-    var $navigationList = $el.closest('.idsk-in-page-navigation__list');
-    var $module = this.$module;
-    var $linkPanelButton = $module.querySelector('.idsk-in-page-navigation__link-panel');
-
-    if ($navigationList == null) {
-        e.stopPropagation(); // prevent bubbling
-        $module.classList.remove('idsk-in-page-navigation--expand');
-        $linkPanelButton.addEventListener('click', $module.boundHandleClickLinkPanel, true);
-        document.removeEventListener('click', $module.boundCheckCloseClick, true);
-    }
-};
-
-/**
- * When the user scrolls down from the top of the document, set position to fixed
- */
-InPageNavigation.prototype.scrollFunction = function () {
-    var $module = this.$module;
-    var $arrTitlesAndElems = this.$arrTitlesAndElems;
-    var $parentModule = $module.parentElement;
-    var $navTopPosition = $parentModule.offsetTop - 55; // padding
-    var $links = $module.querySelectorAll('.idsk-in-page-navigation__list-item');
-
-    if (window.pageYOffset <= $navTopPosition) {
-        $module.classList.remove('idsk-in-page-navigation--sticky');
-    } else {
-        $module.classList.add('idsk-in-page-navigation--sticky');
     }
 
-    if (this.$module.labelChanged) {
-        this.$module.labelChanged = false;
-    } else if ($module.classList.contains('idsk-in-page-navigation--sticky')) {
-        var $self = this;
-        $arrTitlesAndElems.some(function ($item, $index) {
-            if ($item.el.offsetTop >= window.scrollY && $item.el.offsetTop <= (window.scrollY + window.innerHeight)) {
-                $self.changeCurrentLink($links[$index]);
+    $module.boundCheckBlurLanguageSwitcherClick = this.checkBlurLanguageSwitcherClick.bind(this);
 
-                return true
+    // check for e-goverment button
+    var $eGovermentButtons = $module.querySelectorAll('.idsk-header-web__brand-gestor-button');
+    this.$eGovermentSpacer = $module.querySelector('.idsk-header-web__brand-spacer');
+    if ($eGovermentButtons.length > 0) {
+        // Handle $eGovermentButton click event
+        nodeListForEach($eGovermentButtons, function ($eGovermentButton) {
+            $eGovermentButton.addEventListener('click', this.handleEgovermentClick.bind(this));
+        }.bind(this));  
+    }
+
+    // check for menu items
+    var $menuList = $module.querySelector('.idsk-header-web__nav-list');
+    var $menuItems = $module.querySelectorAll('.idsk-header-web__nav-list-item-link');
+    if ($menuItems && $menuList) {
+        // Handle $menuItem click events
+        nodeListForEach($menuItems, function ($menuItem) {
+            $menuItem.addEventListener('click', this.handleSubmenuClick.bind(this));
+            if($menuItem.parentElement.querySelector('.idsk-header-web__nav-submenu') || $menuItem.parentElement.querySelector('.idsk-header-web__nav-submenulite')){
+                $menuItem.parentElement.lastElementChild.addEventListener('keydown', this.menuTabbing.bind(this));
+                $menuItem.parentElement.addEventListener('keydown', this.menuEscPressed.bind(this));
             }
-        });
-    } else {
-        this.changeCurrentLink($links[0]);
+        }.bind(this));
+    }
+
+    // check for mobile menu button
+    var $menuButton = $module.querySelector('.idsk-header-web__main-headline-menu-button');
+    if ($menuButton) {
+        $menuButton.addEventListener('click', this.showMobileMenu.bind(this));
+        this.menuBtnText = $menuButton.innerText.trim();
+    }
+
+    $module.boundCheckBlurMenuItemClick = this.checkBlurMenuItemClick.bind(this);
+};
+
+/**
+ * Handle close banner
+ * @param {object} e
+ */
+ HeaderWeb.prototype.handleCloseBanner = function (e) {
+    var $closeButton = e.target || e.srcElement;
+    var $banner = $closeButton.closest('.idsk-header-web__banner');
+    $banner.classList.add('idsk-header-web__banner--hide');
+};
+
+/**
+ * Handle open/hide language switcher
+ * @param {object} e
+ */
+HeaderWeb.prototype.handleLanguageSwitcherClick = function (e) {
+    var $toggleButton = e.target || e.srcElement;
+    this.$activeSearch = $toggleButton.closest('.idsk-header-web__brand-language');
+    toggleClass(this.$activeSearch, 'idsk-header-web__brand-language--active');
+    if(this.$activeSearch.classList.contains('idsk-header-web__brand-language--active')){
+        this.$activeSearch.firstElementChild.setAttribute('aria-expanded', 'true');
+        this.$activeSearch.firstElementChild.setAttribute('aria-label',  this.$activeSearch.firstElementChild.getAttribute('data-text-for-hide'));
+    }else{
+        this.$activeSearch.firstElementChild.setAttribute('aria-expanded', 'false');
+        this.$activeSearch.firstElementChild.setAttribute('aria-label',  this.$activeSearch.firstElementChild.getAttribute('data-text-for-show'));
+    }
+    document.addEventListener('click', this.$module.boundCheckBlurLanguageSwitcherClick, true);
+};
+
+HeaderWeb.prototype.checkBlurLanguageSwitcherClick = function (e) {
+    if(!(e.target.classList.contains('idsk-header-web__brand-language-button'))){
+        this.$activeSearch.classList.remove('idsk-header-web__brand-language--active');
+        this.$activeSearch.firstElementChild.setAttribute('aria-expanded', 'false');
+        this.$activeSearch.firstElementChild.setAttribute('aria-label',  this.$activeSearch.firstElementChild.getAttribute('data-text-for-show'));
+        document.removeEventListener('click', this.$module.boundCheckBlurLanguageSwitcherClick, true); 
     }
 };
 
-InPageNavigation.prototype.changeCurrentLink = function (el) {
-    var $module = this.$module;
-    var $currItem = el.closest('.idsk-in-page-navigation__list-item');
-    var $articleTitle = $currItem.querySelector('.idsk-in-page-navigation__link-title');
-    var $items = $module.querySelectorAll('.idsk-in-page-navigation__list-item');
-    var $linkPanelText = $module.querySelector('.idsk-in-page-navigation__link-panel-button');
-
-    $items.forEach(function ($item) {
-        $item.classList.remove('idsk-in-page-navigation__list-item--active');
-    });
-    $currItem.classList.add('idsk-in-page-navigation__list-item--active');
-    $linkPanelText.innerText = $articleTitle.innerText;
-
-    // let active item be always visible
-    $currItem.scrollIntoView({
-      block: "nearest",
-      inline: "nearest"
-    });
+HeaderWeb.prototype.handleBackTabbing = function (e) {
+    //shift was down when tab was pressed
+    if(e.shiftKey && e.keyCode == 9 && document.activeElement == this.$toggleLanguageSwitcher) { 
+        this.$toggleLanguageSwitcher.parentNode.classList.remove('idsk-header-web__brand-language--active');  
+    }
 };
 
-return InPageNavigation;
+/**
+ * Handle open/hide e-goverment statement
+ * @param {object} e
+ */
+ HeaderWeb.prototype.handleEgovermentClick = function (e) {
+    var $eGovermentButtons = this.$module.querySelectorAll('.idsk-header-web__brand-gestor-button');
+    var $eGovermentDropdown = this.$module.querySelector('.idsk-header-web__brand-dropdown');
+    toggleClass($eGovermentDropdown, 'idsk-header-web__brand-dropdown--active');
+    toggleClass(this.$eGovermentSpacer, 'idsk-header-web__brand-spacer--active');
+    nodeListForEach($eGovermentButtons, function ($eGovermentButton) {
+        toggleClass($eGovermentButton, 'idsk-header-web__brand-gestor-button--active');
+        if($eGovermentButton.classList.contains('idsk-header-web__brand-gestor-button--active')){
+            $eGovermentButton.setAttribute('aria-expanded', 'true');
+            $eGovermentButton.setAttribute('aria-label', $eGovermentButton.getAttribute('data-text-for-hide'));          
+        }else{
+            $eGovermentButton.setAttribute('aria-expanded', 'false');
+            $eGovermentButton.setAttribute('aria-label', $eGovermentButton.getAttribute('data-text-for-show'));
+        }
+    }.bind(this));
+};
+
+
+/**
+ * Handle open/hide submenu
+ * @param {object} e 
+ */
+HeaderWeb.prototype.handleSubmenuClick = function (e) {
+    var $srcEl = e.target || e.srcElement;
+    var $toggleButton = $srcEl.closest('.idsk-header-web__nav-list-item');
+    var $currActiveItem = this.$module.querySelector('.idsk-header-web__nav-list-item--active');
+    
+    if($currActiveItem && $currActiveItem.isEqualNode($toggleButton)){
+        $currActiveItem.classList.remove('idsk-header-web__nav-list-item--active');
+        if($toggleButton.childNodes[3]){
+          $currActiveItem.childNodes[1].setAttribute('aria-expanded', 'false');
+        $toggleButton.childNodes[1].setAttribute('aria-label', $toggleButton.childNodes[1].getAttribute('data-text-for-show'));   
+        }
+    }else{
+        if($currActiveItem){
+            $currActiveItem.classList.remove('idsk-header-web__nav-list-item--active');
+        }
+        toggleClass($toggleButton, 'idsk-header-web__nav-list-item--active'); 
+
+        if($toggleButton.childNodes[3] && $toggleButton.classList.contains('idsk-header-web__nav-list-item--active')) {   
+            $toggleButton.childNodes[1].setAttribute('aria-expanded', 'true'); 
+            $toggleButton.childNodes[1].setAttribute('aria-label', $toggleButton.childNodes[1].getAttribute('data-text-for-hide'));
+        }
+    }
+
+    document.addEventListener('click', this.$module.boundCheckBlurMenuItemClick.bind(this), true);
+};
+
+/**
+ * Remove active class from menu when user leaves menu with tabbing
+ */
+ HeaderWeb.prototype.menuTabbing = function (e) {
+    var isTabPressed = (e.key === 'Tab' || e.keyCode === 9);
+
+    if (!isTabPressed) {
+        return;
+    }
+
+    var $submenuList = e.srcElement.parentElement.parentElement;
+    var $activeItem = $submenuList.closest('.idsk-header-web__nav-list-item');
+    // shift + tab
+    if (e.shiftKey) {
+        if (document.activeElement === $submenuList.firstElementChild.firstElementChild) {
+            $activeItem.classList.remove('idsk-header-web__nav-list-item--active');
+            $activeItem.childNodes[1].setAttribute('aria-expanded', 'false'); 
+            $activeItem.childNodes[1].setAttribute('aria-label', $activeItem.childNodes[1].getAttribute('data-text-for-show')); 
+        }
+    // tab
+    } else if (document.activeElement === $submenuList.lastElementChild.lastElementChild) {
+        $activeItem.classList.remove('idsk-header-web__nav-list-item--active');
+        $activeItem.childNodes[1].setAttribute('aria-expanded', 'false'); 
+        $activeItem.childNodes[1].setAttribute('aria-label', $activeItem.childNodes[1].getAttribute('data-text-for-show')); 
+    } 
+};
+
+/**
+ * Remove active class from menu when user leaves menu with esc
+ */
+ HeaderWeb.prototype.menuEscPressed = function (e) {
+    if(e.key === "Escape") {
+        var $menuList = e.srcElement.parentElement.parentElement;
+        if($menuList.classList.contains('idsk-header-web__nav-submenulite-list') || $menuList.classList.contains('idsk-header-web__nav-submenu-list')){
+            $menuList = $menuList.closest('.idsk-header-web__nav-list');
+        }
+        var $activeItem = $menuList.querySelector('.idsk-header-web__nav-list-item--active');
+        $activeItem.classList.remove('idsk-header-web__nav-list-item--active');
+        $activeItem.childNodes[1].setAttribute('aria-expanded', 'false'); 
+        $activeItem.childNodes[1].setAttribute('aria-label', $activeItem.childNodes[1].getAttribute('data-text-for-show')); 
+    }
+ };
+
+/**
+ * handle click outside menu or "blur" the item link
+ */
+HeaderWeb.prototype.checkBlurMenuItemClick = function (e) {
+    var $currActiveItem = this.$module.querySelector('.idsk-header-web__nav-list-item--active');
+    if($currActiveItem && !(e.target.classList.contains('idsk-header-web__nav-list-item-link'))){
+        $currActiveItem.classList.remove('idsk-header-web__nav-list-item--active');
+        if( $currActiveItem.childNodes[3] ){
+           $currActiveItem.childNodes[1].setAttribute('aria-expanded', 'false'); 
+           $currActiveItem.childNodes[1].setAttribute('aria-label', $currActiveItem.childNodes[1].getAttribute('data-text-for-show')); 
+        }
+        document.removeEventListener('click', this.$module.boundCheckBlurMenuItemClick, true); 
+    }
+};
+
+/**
+ * Show mobile menu
+ * @param {object} e
+ */
+HeaderWeb.prototype.showMobileMenu = function (e) {
+    var closeText = this.menuBtnText ? 'Zavrieť' : '';
+    var $menuButton = this.$module.querySelector('.idsk-header-web__main-headline-menu-button');
+    var $mobileMenu = this.$module.querySelector('.idsk-header-web__nav');
+    toggleClass($mobileMenu, 'idsk-header-web__nav--mobile');
+    toggleClass($menuButton, 'idsk-header-web__main-headline-menu-button--active');
+    if(!$menuButton.classList.contains('idsk-header-web__main-headline-menu-button--active')){
+        $menuButton.setAttribute('aria-expanded', 'false'); 
+        $menuButton.setAttribute('aria-label', $menuButton.getAttribute('data-text-for-show'));
+    }else{
+        $menuButton.setAttribute('aria-expanded', 'true'); 
+        $menuButton.setAttribute('aria-label', $menuButton.getAttribute('data-text-for-hide'));
+    }
+    var buttonIsActive = $menuButton.classList.contains('idsk-header-web__main-headline-menu-button--active');
+
+    $menuButton.childNodes[0].nodeValue = buttonIsActive ? closeText : this.menuBtnText;
+};
+
+return HeaderWeb;
 
 })));
