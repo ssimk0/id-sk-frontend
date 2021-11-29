49f8a95b1107285929873aee6fbc0239f6e85727
Tibor Dulovec <tibor.dulovec@gmail.com>
2021-11-29 12:52:53 +0100

    Minor changes

--- app/app.js
+++ app/app.js
@@ -62,6 +62,7 @@ module.exports = (options) => {
     next()
   })
 
+  // Set up middleware to serve static assets
   app.use('/public', express.static(configPaths.public))
 
   app.use('/docs', express.static(configPaths.sassdoc))
